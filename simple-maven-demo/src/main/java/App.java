7b303cc0c9c819d3b7777a4000833985e21ac6bf
Sujay Chandake <chandakesujay@gmail.com>
2025-12-10 14:06:31 +0530

    Update App.java

--- simple-maven-demo/src/main/java/App.java
+++ simple-maven-demo/src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Build 🚀");
+        System.out.println("Action Triggered! 🚀");
     }
 }
