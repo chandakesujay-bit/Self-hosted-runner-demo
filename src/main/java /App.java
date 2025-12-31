430d653aae874da7981c598041f0506f60bd8379
Sujay Chandake <chandakesujay@gmail.com>
2025-12-31 13:33:54 +0530

    Update App.java

--- src/main/java /App.java
+++ src/main/java /App.java
@@ -1,6 +1,12 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Runner triggered 🚀");
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class App {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Runner triggered 🚀");
     }
 }
